fddebb73a5141888d9eb771811cb4c87c1d2e1a0
Kostitsina Elizaveta <kostitsinaliza@mail.ru>
2021-04-08 21:50:15 +0300

    LaunchScreen

--- Swiftbook TableViewApp/TableViewController.swift
+++ Swiftbook TableViewApp/TableViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 class TableViewController: UITableViewController {
 
     
-    let myContacts = [
+    let names = [
         
         "Emma Smith","Jessica Jones",
         "James Evans", "Michael Moore",
         "Alexander Miller", "Elizabeth Wilson"
+    ]
+    
+    let numbers = [
+        "+(495) 189365" ,"+(495) 892333",
+        "+(495) 200321","+(495) 102935",
+        "+(495) 2393115","+(495) 109235"
     ]
     
     override func viewDidLoad() {
@@ -23,32 +29,37 @@ class TableViewController: UITableViewController {
 
     }
 
-
+    //MARK: TableViewDataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        myContacts.count
+        names.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! Cell
         
-        cell.nameLabel.text = myContacts[indexPath.row]
-        cell.photoImage.image = UIImage(named: myContacts[indexPath.row])
-    
+        cell.nameLabel.text = names[indexPath.row]
+        
+        cell.photoImage.layer.cornerRadius = cell.photoImage.frame.height / 7
+        cell.photoImage.image = UIImage(named: names[indexPath.row])
+        
+        cell.numberLabel.text = numbers[indexPath.row]
         return cell
     }
     
+    //MARK: TableViewDelegate
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        70
+         return 90
+        
     }
-    
-    
 }
 
-
+    //MARK: Class Cell
 class Cell: UITableViewCell {
     
-   
+    @IBOutlet weak var numberLabel: UILabel!
+    
     @IBOutlet weak var photoImage: UIImageView!
     
     @IBOutlet weak var nameLabel: UILabel!
