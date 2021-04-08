d35f110b3b3f86c73a3341fd6a3aea670bb658ec
Kostitsina Elizaveta <kostitsinaliza@mail.ru>
2021-04-08 17:56:54 +0300

    Initial

---
+++ Swiftbook TableViewApp/TableViewController.swift
@@ -0,0 +1,56 @@
+//
+//  TableViewController.swift
+//  Swiftbook TableViewApp
+//
+//  Created by Костицина Елизавета Константиновна on 07.04.2021.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    
+    let myContacts = [
+        
+        "Emma Smith","Jessica Jones",
+        "James Evans", "Michael Moore",
+        "Alexander Miller", "Elizabeth Wilson"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        myContacts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! Cell
+        
+        cell.nameLabel.text = myContacts[indexPath.row]
+        cell.photoImage.image = UIImage(named: myContacts[indexPath.row])
+    
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        80
+    }
+    
+    
+}
+
+
+class Cell: UITableViewCell {
+    
+   
+    @IBOutlet weak var photoImage: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
