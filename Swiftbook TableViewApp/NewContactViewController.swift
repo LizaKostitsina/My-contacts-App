a26bc3d932c404a625fa0d3435e3362af7995cf7
Kostitsina Elizaveta <kostitsinaliza@mail.ru>
2021-04-10 17:49:05 +0300

    Adding ActionSheet

--- Swiftbook TableViewApp/NewContactViewController.swift
+++ Swiftbook TableViewApp/NewContactViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class NewContactViewController: UITableViewController {
 
+    @IBOutlet weak var photoImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +21,23 @@ class NewContactViewController: UITableViewController {
     //Скрываем клавиатуру при нажатии на экран
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) {_ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
             
         } else {
             view.endEditing(true)
@@ -34,5 +53,27 @@ extension NewContactViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+//MARK: Work with Image
+extension NewContactViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func chooseImagePicker(source: UIImagePickerController.SourceType){
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        photoImage.image = info[.editedImage] as? UIImage
+        photoImage.contentMode = .scaleAspectFill
+        photoImage.clipsToBounds = true
+        dismiss(animated: true, completion: nil)
     }
 }
